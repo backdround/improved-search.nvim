5aba8087ba8de4f3cc427420c572a044237de89d
Vlad Chepaykyn <backdround@yandex.ru>
2023-10-14 23:58:07 +0300

    Perform search as an operator. not just a function

--- lua/improved-search/init.lua
+++ lua/improved-search/init.lua
@@ -1,4 +1,41 @@
+--# selene: allow(unused_variable)
 local utils = require("improved-search.utils")
+
+function Improved_search_selected_text(selection_type)
+  -- Leaves visual mode
+  local escape = vim.api.nvim_replace_termcodes("<esc>", true, false, true)
+  vim.api.nvim_feedkeys(escape, "nx", false)
+
+  -- Gets text to search
+  local selected_lines = utils.get_visual_selection_range(selection_type)
+
+  -- Choose special symbols to escape
+  local special_symbols = "^$\\"
+  if vim.api.nvim_get_option("magic") then
+    special_symbols = "*^$.~[]\\"
+  end
+
+  -- Escape all special_symbols
+  for index, line in ipairs(selected_lines) do
+    selected_lines[index] = vim.fn.escape(line, special_symbols)
+  end
+
+  -- Join all lines to text for the searching
+  local text_to_search = table.concat(selected_lines, "\\n")
+  if selection_type == "line" then
+    text_to_search = "^" .. text_to_search .. "\\n"
+  elseif selection_type == "block" then
+    text_to_search = ".*" .. table.concat(selected_lines, ".*\\n.*") .. ".*"
+  end
+
+  -- Sets cursor to left part of selected text (for next search conviniece)
+  local start_row, start_column = utils.get_mark("[")
+  vim.api.nvim_win_set_cursor(0, { start_row, start_column })
+
+  -- Sets the selected text as a search text
+  vim.fn.setreg("/", text_to_search)
+  vim.opt.hlsearch = true
+end
 
 local function search_current_word_without_moving()
   local current_word = vim.fn.expand("<cword>")
@@ -16,59 +53,49 @@ local function search_current_word_without_moving()
   vim.fn.search(current_word, "cb")
 end
 
-local function search_stable_next()
+local function search_stable_next(count)
+  if not count then
+    count = vim.v.count1
+  end
   if vim.v.searchforward == 1 then
-    utils.normal(vim.v.count1 .. "n")
+    utils.normal(count .. "n")
   else
-    utils.normal(vim.v.count1 .. "N")
+    utils.normal(count .. "N")
   end
 end
 
-local function search_stable_previous()
+local function search_stable_previous(count)
+  if not count then
+    count = vim.v.count1
+  end
   if vim.v.searchforward == 0 then
-    utils.normal(vim.v.count1 .. "n")
+    utils.normal(count .. "n")
   else
-    utils.normal(vim.v.count1 .. "N")
+    utils.normal(count .. "N")
   end
 end
 
 local function search_selected_text()
-  -- Leaves visual mode
-  local escape = vim.api.nvim_replace_termcodes("<esc>", true, false, true)
-  vim.api.nvim_feedkeys(escape, "nx", false)
-
-  -- Gets text to search
-  local selected_lines = utils.get_visual_selection_range()
-
-  local special_symbols = "^$\\"
-  if vim.api.nvim_get_option("magic") then
-    special_symbols = "*^$.~[]\\"
-  end
-
-  for index, line in ipairs(selected_lines) do
-    selected_lines[index] = vim.fn.escape(line, special_symbols)
-  end
-  local text_to_search = table.concat(selected_lines, "\\n")
-
-  -- Sets cursor to left part of selected text (for next search conviniece)
-  local start_row, start_column = utils.get_mark("<")
-  vim.api.nvim_win_set_cursor(0, { start_row + 1, start_column })
-
-  -- Sets the selected text as a search text
-  vim.fn.setreg("/", text_to_search)
-  vim.opt.hlsearch = true
+  vim.go.operatorfunc = "v:lua.Improved_search_selected_text"
+  vim.api.nvim_feedkeys("g@", "n", false)
 end
 
 -- "*" in visual mode, that can search multiline-pattern
 local function search_selected_text_next()
+  local saved_count = vim.v.count1
   search_selected_text()
-  search_stable_next()
+  vim.schedule(function()
+    search_stable_next(saved_count)
+  end)
 end
 
 -- "#" in visual mode, that can search multiline-pattern
 local function search_selected_text_previous()
+  local saved_count = vim.v.count1
   search_selected_text()
-  search_stable_previous()
+  vim.schedule(function()
+    search_stable_previous(saved_count)
+  end)
 end
 
 return {
--- lua/improved-search/utils.lua
+++ lua/improved-search/utils.lua
@@ -17,29 +17,32 @@ M.normal = function(...)
 end
 
 M.get_mark = function(mark)
-  local line, column = unpack(vim.api.nvim_buf_get_mark(0, mark))
-
-  -- Checks that column unsigned(-1)
-  local line_string = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-  if column > line_string:len() then
-    column = line_string:len()
-  end
-
-  return line - 1, column
+  return unpack(vim.api.nvim_buf_get_mark(0, mark))
 end
 
-M.get_visual_selection_range = function()
-  local start_row, start_column = M.get_mark("<")
-  local end_row, end_column = M.get_mark(">")
-  local selected_lines = vim.api.nvim_buf_get_text(
-    0,
-    start_row,
-    start_column,
-    end_row,
-    end_column + 1,
-    {}
-  )
-  return selected_lines
+M.get_visual_selection_range = function(selection_type)
+  local start_row, start_column = M.get_mark("[")
+  local end_row, end_column = M.get_mark("]")
+
+  if selection_type == "line" then
+    return vim.api.nvim_buf_get_lines(0, start_row - 1, end_row, true)
+  elseif selection_type == "char" then
+    return vim.api.nvim_buf_get_text(
+      0,
+      start_row - 1,
+      start_column,
+      end_row - 1,
+      end_column + 1,
+      {}
+    )
+  elseif selection_type == "block" then
+    local selected_lines =
+      vim.api.nvim_buf_get_lines(0, start_row - 1, end_row, true)
+    for i, line in ipairs(selected_lines) do
+      selected_lines[i] = line:sub(start_column + 1, end_column + 1)
+    end
+    return selected_lines
+  end
 end
 
 return M
